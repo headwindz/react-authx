b2dc0779e2235fe237e2ece39c540a454952813e
n0ruSh <michael2ib1989@gmail.com>
2022-01-07 00:09:11 +0800

    fix: fix test cases

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export { default as Foo } from './Foo'
+import AuthContext from './hoc/authContext';
+import AuthConsumer, { setCustomValidator, hasAuth } from './hoc/authConsumer';
+import { AuthWrapper } from './hoc/authWrapper';
+
+const AuthProvider = AuthContext.Provider;
+export { AuthProvider, AuthConsumer, setCustomValidator, AuthWrapper, hasAuth };
